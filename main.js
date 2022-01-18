a29666f9e6c27db0e1a31fc94da9fa3c10925cab
wookshin <wookshins@gmail.com>
2022-01-19 04:51:13 +0900

    JS: arrow up

--- main.js
+++ main.js
@@ -41,6 +41,22 @@ document.addEventListener('scroll', () => {
   homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
 })
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+window.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+})
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+  
+})
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: "smooth"});
