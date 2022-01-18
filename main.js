3a9c54e95838d97d7db611e6be72cfa4f6f6e850
wookshin <wookshins@gmail.com>
2022-01-19 07:35:42 +0900

    JS: Remove navbar active & Modify active category btn

--- main.js
+++ main.js
@@ -60,12 +60,18 @@ const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 
-workBtnContainer.addEventListener("click", ({ target }) => {
-  const filter = target.dataset.filter || target.parentNode.dataset.filter;
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
 
   if (filter == null) {
     return;
   }
+
+  // Remove selection from the previous item and select the new one. 
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
 
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
