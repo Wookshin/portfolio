dbfec6b1da750afcd23324dcaded8a93c7cb06ee
wookshin <wookshins@gmail.com>
2022-01-19 08:21:42 +0900

    JS: navbar menu toggle & css retouching

--- main.js
+++ main.js
@@ -21,9 +21,17 @@ navbarMenu.addEventListener("click", (e) => {
   if (link == null) {
     return;
   }
-
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
+
 
 // Handle click on "contact me" button on home
 let contactBtn = document.querySelector(".home__contact");
